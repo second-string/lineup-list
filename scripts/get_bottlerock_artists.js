2e1b1e46cb2e38071582f568f6f0cb900a329071
dot4qu <dot4qu@virginia.edu>
2020-04-26 10:55:30 -0700

    Add script for pulling in bottlerock artists, update Bottlerock_2020.txt with full lineup

---
+++ scripts/get_bottlerock_artists.js
@@ -0,0 +1,31 @@
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+async function run() {
+    const url = "https://www.bottlerocknapavalley.com/2020artists/";
+    const response = await fetch(url);
+    const html = await response.text();
+    const $ = cheerio.load(html);
+    const artists = [];
+    // $(".artist-name").each((i, e) => {
+    //     console.log($(this).text());
+    // });
+
+    // the .text() called on the span elements is empty for some reason, so just hack out the artists manually
+    const domArtists = $(".artist-name");
+    for (var i in Object.keys(domArtists)) {
+        const domArtist = domArtists[i];
+        if (domArtist === undefined) {
+            continue;
+        }
+
+        artists.push(domArtist.children[0].data);
+    }
+
+    console.log(artists);
+    const artistsStr = artists.join("\r\n");
+    fs.writeFileSync("Bottlerock_2020_full.txt", artistsStr);
+}
+
+run()
