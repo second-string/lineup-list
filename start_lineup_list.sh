2a3e2e978e668ab98a99ff880c24e9e1a7204918
dot4qu <dot4qu@virginia.edu>
2021-05-08 12:57:38 -0700

    remove warm call from startup script

--- start_lineup_list.sh
+++ start_lineup_list.sh
@@ -8,5 +8,5 @@ $NPM run build
 # for every supported festival and year listed in the file. As this number
 # grows I'll need to make it better since it already 409s and backs off for
 # just two.
-$NODE dist/warm-cache-for-festival.js
+#$NODE dist/warm-cache-for-festival.js
 $NPM start
