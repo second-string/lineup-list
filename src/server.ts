f39db3acaa922cacfa9442667e265ceb71fed76c
dot4qu <dot4qu@virginia.edu>
2021-05-22 13:00:23 -0700

    Add instructions for full env var setup to readme, add support for http-only mode (not functional  around spotify auth callback)

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,16 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import handlebars from "express-handlebars";
 import fs from "fs";
+import http from "http";
 import https from "https";
-// import http from "http";
 import redis from "redis";
 import uuid from "uuid/v4";
 
 import apiRouter from "./routes/api";
 import pageRouter from "./routes/pages";
+
+const forceHttp: boolean =
+    process.env.LINEUPLIST_FORCE_HTTP && process.env.LINEUPLIST_FORCE_HTTP === "true" ? true : false;
 
 const redisClient = redis.createClient();
 redisClient.on("error", (err: Error) => console.log(err));
@@ -19,7 +22,7 @@ if (!process.env.DEPLOY_STAGE || process.env.DEPLOY_STAGE === '') {
     console.log("Need to source setup_env.sh to set env variables");
     process.exit(1);
 }
-const port = process.env.DEPLOY_STAGE === 'PROD' ? 7443 : 443;
+const port = process.env.DEPLOY_STAGE === 'PROD' ? 7443 : forceHttp ? 80 : 443;
 
 app.engine("handlebars", handlebars());
 app.set("view engine", "handlebars");
@@ -46,7 +49,8 @@ app.use((req: express.Request, res: express.Response, next: express.NextFunction
 
 // Set canonical for hbs main template to stick in head meta tag
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.locals.canonicalUrl = `https://${req.get('host')}${req.originalUrl}`;
+    const protocol          = forceHttp ? "http" : "https";
+    res.locals.canonicalUrl = `${protocol}://${req.get('host')}${req.originalUrl}`;
     next();
 });
 
@@ -54,24 +58,30 @@ app.use(apiRouter(redisClient))
 app.use(pageRouter(redisClient));
 
 // HTTPS certs
-let creds = {};
-if (process.env.DEPLOY_STAGE === "PROD") {
-    if (!process.env.PROD_SSL_KEY_PATH || !process.env.PROD_SSL_CERT_PATH || !process.env.PROD_SSL_CA_CERT_PATH) {
-        console.log("SSL cert env variables not set. Source the setup_env.sh script");
-        process.exit(1);
+if (forceHttp) {
+    console.log("LINEUPLIST_FORCE_HTTP set, skipping certs and starting an http ONLY local server");
+    const httpServer = http.createServer(app);
+    httpServer.listen(port);
+} else {
+    let creds = {};
+    if (process.env.DEPLOY_STAGE === "PROD") {
+        if (!process.env.PROD_SSL_KEY_PATH || !process.env.PROD_SSL_CERT_PATH || !process.env.PROD_SSL_CA_CERT_PATH) {
+            console.log("SSL cert env variables not set. Source the setup_env.sh script");
+            process.exit(1);
+        }
+
+        const key  = fs.readFileSync(process.env.PROD_SSL_KEY_PATH);
+        const cert = fs.readFileSync(process.env.PROD_SSL_CERT_PATH);
+        const ca   = fs.readFileSync(process.env.PROD_SSL_CA_CERT_PATH);
+        creds      = {key, cert, ca};
+    } else {
+        console.log("Running server locally using local self-signed cert");
+        const localKey  = fs.readFileSync(__dirname + "/../lineuplist-selfsigned-key.pem", "utf-8");
+        const localCert = fs.readFileSync(__dirname + "/../lineuplist-selfsigned-cert.pem", "utf-8");
+        creds           = {key : localKey, cert : localCert};
     }
 
-    const key  = fs.readFileSync(process.env.PROD_SSL_KEY_PATH);
-    const cert = fs.readFileSync(process.env.PROD_SSL_CERT_PATH);
-    const ca   = fs.readFileSync(process.env.PROD_SSL_CA_CERT_PATH);
-    creds      = {key, cert, ca};
-} else {
-    console.log("Running server locally using local self-signed cert");
-    const localKey  = fs.readFileSync(__dirname + "/../lineuplist-selfsigned-key.pem", "utf-8");
-    const localCert = fs.readFileSync(__dirname + "/../lineuplist-selfsigned-cert.pem", "utf-8");
-    creds           = {key : localKey, cert : localCert};
+    const httpsServer = https.createServer(creds, app);
+    console.log(`Starting server listening at ${port}`);
+    httpsServer.listen(port);
 }
-
-const httpsServer = https.createServer(creds, app);
-console.log(`Starting server listening at ${port}`);
-httpsServer.listen(port);
