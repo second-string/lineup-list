e73e5cc3a710042d6c3b742fa1f2c591194cfdff
James Leece <james.leece@gmail.com>
2022-06-28 15:31:53 +0100

    Don't connect to setlist.fm if no API key

--- src/setlist-fm-helper.ts
+++ src/setlist-fm-helper.ts
@@ -3,6 +3,11 @@ import {instrumentCall} from "./helpers";
 const baseUrl = "https://api.setlist.fm/rest/1.0/";
 
 async function getArtistSetlists(artistMbid: string): Promise<SetlistFmSetlist[]> {
+
+    if (process.env.DEPLOY_STAGE !== 'PROD' &&
+       (!process.env.SETLIST_FM_API_KEY || process.env.SETLIST_FM_API_KEY === ""))
+        return [];
+
     const url  = `${baseUrl}artist/${artistMbid}/setlists`;
     const opts = {
         headers : {
