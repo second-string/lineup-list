7f9444267b87857cd5d3261cd6d93e603d9dae6c
dot4qu <brian.team.jr@gmail.com>
2022-08-08 09:27:41 -0400

    Add bourbon and beyond lineup

--- src/constants.ts
+++ src/constants.ts
@@ -68,4 +68,5 @@ export const supportedFestivals: Festival[] = [
     {display_name : "Leeds Festival", years : [ 2022 ], name : "leeds", region : "eu"},
     {display_name : "Reading Festival", years : [ 2022 ], name : "reading", region : "eu"},
     {display_name : "Life Is Beautiful", years : [ 2022 ], name : "lifeisbeautiful", region : "am"},
+    {display_name : "Bourbon and Beyond", years : [ 2022 ], name : "bourbonandbeyond", region : "am"},
 ];
