03c8df7c861e3c6c78ef358623ab4943aa5dab7e
dot4qu <brian.team.jr@gmail.com>
2023-02-04 09:15:00 +0100

    when we were young 2023 lineup

--- src/constants.ts
+++ src/constants.ts
@@ -70,7 +70,7 @@ export const supportedFestivals: Festival[] = [
     {display_name : "Reading Festival", years : [ 2022 ], name : "reading", region : "eu"},
     {display_name : "Life Is Beautiful", years : [ 2022 ], name : "lifeisbeautiful", region : "am"},
     {display_name : "Bourbon and Beyond", years : [ 2022 ], name : "bourbonandbeyond", region : "am"},
-    {display_name : "When We Were Young", years : [ 2022 ], name : "wwwy", region : "am"},
+    {display_name : "When We Were Young", years : [ 2023, 2022 ], name : "wwwy", region : "am"},
     {display_name : "Primavera Sound Barcelona", years : [ 2023, 2022 ], name : "primavera", region : "eu"},
     {display_name : "Regenerate", years : [ 2023 ], name : "regenerate", region : "am"},
 ];
