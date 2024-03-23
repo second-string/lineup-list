1060790c41b08ce0dee05fb09d9069358e3e0226
second-string <brian.team.jr@gmail.com>
2024-03-23 16:22:34 +0100

    glasto 2024 lineup

--- src/constants.ts
+++ src/constants.ts
@@ -82,4 +82,5 @@ export const supportedFestivals: Festival[] = [
     {display_name : "Kilby Block Party", years : [ 2024 ], name : "kilby", region : "am"},
     {display_name : "Faster Horses", years : [ 2024 ], name : "horses", region : "am"},
     {display_name : "Oceans Calling", years : [ 2024 ], name : "oceanscalling", region : "am"},
+    {display_name : "Glastonbury", years : [ 2024 ], name : "glasto", region : "eu"},
 ];
