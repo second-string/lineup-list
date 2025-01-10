1d6a9b615cc8d3a0cc2a91a4e316df0fc501c447
second-string <brian.team.jr@gmail.com>
2025-01-10 09:58:08 +0100

    Add boston calling 2025

--- src/constants.ts
+++ src/constants.ts
@@ -88,4 +88,5 @@ export const supportedFestivals: Festival[] = [
     {display_name : "Project Glow", years : [ 2024 ], name : "projectglow", region : "am"},
     {display_name : "Lost Lands", years : [ 2024 ], name : "lostlands", region : "am"},
     {display_name : "FORM", years : [ 2024 ], name : "form", region : "am"},
+    {display_name : "Boston Calling", years : [ 2025 ], name : "bostoncalling", region : "am"},
 ];
