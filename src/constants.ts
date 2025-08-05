86509a1f95a7a011fc60c63e0745c4267e5eeebd
second-string <brian.team.jr@gmail.com>
2025-08-05 20:06:12 +0200

    Voodoo Village lineupe 2025

--- src/constants.ts
+++ src/constants.ts
@@ -93,4 +93,5 @@ export const supportedFestivals: Festival[] = [
     {display_name : "Louder Than Life", years : [ 2025 ], name : "louderthanlife", region : "am"},
     {display_name : "Sonoora", years : [ 2025 ], name : "sonoora", region : "am"},
     {display_name : "Sound Haven", years : [ 2025 ], name : "soundhaven", region : "am"},
+    {display_name : "Voodoo Village", years : [ 2025 ], name : "voodoo", region : "eu"},
 ];
