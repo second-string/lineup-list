920613f0ab84cec29c263f18e43ef6cffdc66f02
second-string <brian.team.jr@gmail.com>
2026-05-21 21:03:53 +0200

    all things go 2026

--- src/constants.ts
+++ src/constants.ts
@@ -86,7 +86,7 @@ export const supportedFestivals: Festival[] = [
     },
     {display_name : "Regenerate", years : [ 2023 ], name : "regenerate", region : "am"},
     {display_name : "Envision", years : [ 2026, 2023 ], name : "envision", region : "am"},
-    {display_name : "All Things Go", years : [ 2023 ], name : "atg", region : "am"},
+    {display_name : "All Things Go", years : [ 2026, 2023 ], name : "atg", region : "am"},
     {display_name : "Lovers & Friends", years : [ 2024, 2023 ], name : "laf", region : "am"},
     {display_name : "Same Same But Different", years : [ 2023 ], name : "ssbd", region : "am"},
     {display_name : "Aftershock", years : [ 2025, 2024, 2023 ], name : "aftershock", region : "am"},
